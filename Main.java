bf45becaa8230b1fb7e009096a10a92a4fb5dbe2
lazerclaw321 <mr.ericfang@gmail.com>
2026-04-28 21:10:46 -0400

    Removed extensions due to timed movement

--- Main.java
+++ Main.java
@@ -14,14 +14,14 @@ public class Main {
     static double scorePosGlobal;
     static int movesCalculated = 0;
     static long startingTime;
-    static final long thinkingTime = 500;
+    static final long thinkingTime = 5000;
 
-    static final int[][] psqPawn = new int[8][8];
-    static final int[][] psqKnight = new int[8][8];
-    static final int[][] psqBishop = new int[8][8];
-    static final int[][] psqQueen = new int[8][8];
-    static final int[][] psqRook = new int[8][8];
-    static final int[][] psqKing = new int[8][8];
+    static int[] psqPawn = new int[64];
+    static int[] psqKnight = new int[64];
+    static int[] psqBishop = new int[64];
+    static int[] psqQueen = new int[64];
+    static int[] psqRook = new int[64];
+    static int[] psqKing = new int[64];
 
     public static char[][] board = new char[8][8];
     public static boolean printMoves = false;
@@ -30,70 +30,67 @@ public class Main {
     public static int moveX = -1, moveY = -1;
     public static GamePanel panel = new GamePanel();
 
-    static {
-        board[0] = new char[]{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'};
-        board[1] = new char[]{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'};
-        board[2] = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-        board[3] = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-        board[4] = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-        board[5] = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
-        board[6] = new char[]{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'};
-        board[7] = new char[]{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'};
-
-        psqPawn[0] = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-        psqPawn[1] = new int[]{5, 0, -10, -20, -20, -10, 0, 5};
-        psqPawn[2] = new int[]{5, 1, -10, 0, 0, -10, 1, 5};
-        psqPawn[3] = new int[]{0, 0, 0, 19, 21, 0, 0, 0};
-        psqPawn[4] = new int[]{5, 5, 5, 25, 25, 5, 5, 5};
-        psqPawn[5] = new int[]{10, 20, 25, 30, 30, 25, 20, 10};
-        psqPawn[6] = new int[]{50, 50, 50, 60, 60, 50, 50, 50};
-        psqPawn[7] = new int[]{800, 800, 800, 800, 800, 800, 800, 800};
-
-        psqKnight[0] = new int[]{-50, -25, -20, -20, -20, -20, -25, -50};
-        psqKnight[1] = new int[]{-25, -10, -1, 5, 5, -1, -10, -25};
-        psqKnight[2] = new int[]{-50, -1, 14, 20, 20, 14, -1, -50};
-        psqKnight[3] = new int[]{-20, 5, 20, 25, 25, 20, 5, -20};
-        psqKnight[4] = new int[]{-20, 5, 20, 25, 25, 20, 5, -20};
-        psqKnight[5] = new int[]{-50, -1, 14, 20, 20, 14, -1, -50};
-        psqKnight[6] = new int[]{-25, -10, -1, 5, 5, -1, -10, -25};
-        psqKnight[7] = new int[]{-50, -25, -20, -20, -20, -20, -25, -50};
-
-        psqBishop[0] = new int[]{-20, -10, -10, -10, -10, -10, -10, -20};
-        psqBishop[1] = new int[]{-10, -5, 0, 5, 5, 0, -5, -10};
-        psqBishop[2] = new int[]{-10, 0, 10, 15, 15, 10, 0, -10};
-        psqBishop[3] = new int[]{-10, 5, 15, 20, 20, 15, 5, -10};
-        psqBishop[4] = new int[]{-10, 5, 15, 20, 20, 15, 5, -10};
-        psqBishop[5] = new int[]{-10, 0, 10, 15, 15, 10, 0, -10};
-        psqBishop[6] = new int[]{-10, -5, 0, 5, 5, 0, -5, -10};
-        psqBishop[7] = new int[]{-20, -10, -10, -10, -10, -10, -10, -20};
-
-        psqQueen[0] = new int[]{-20, -10, -10, -10, -10, -10, -10, -20};
-        psqQueen[1] = new int[]{-10, -5, 0, 5, 5, 0, -5, -10};
-        psqQueen[2] = new int[]{-10, 0, 10, 15, 15, 10, 0, -10};
-        psqQueen[3] = new int[]{-10, 5, 15, 20, 20, 15, 5, -10};
-        psqQueen[4] = new int[]{-10, 5, 15, 20, 20, 15, 5, -10};
-        psqQueen[5] = new int[]{-10, 0, 10, 15, 15, 10, 0, -10};
-        psqQueen[6] = new int[]{-10, -5, 0, 5, 5, 0, -5, -10};
-        psqQueen[7] = new int[]{-20, -10, -10, -10, -10, -10, -10, -20};
-
-        psqRook[0] = new int[]{10, 10, 10, 10, 10, 10, 10, 10};
-        psqRook[1] = new int[]{11, 11, 11, 11, 11, 11, 11, 11};
-        psqRook[2] = new int[]{-1, -1, -1, -1, -1, -1, -1, -1};
-        psqRook[3] = new int[]{-1, -1, -1, -1, -1, -1, -1, -1};
-        psqRook[4] = new int[]{-1, -1, -1, -1, -1, -1, -1, -1};
-        psqRook[5] = new int[]{-1, -1, -1, -1, -1, -1, -1, -1};
-        psqRook[6] = new int[]{-1, -1, -1, -1, -1, -1, -1, -1};
-        psqRook[7] = new int[]{0, 0, 0, 12, 12, 5, 0, 0};
-
-        psqKing[0] = new int[]{-5, -5, -5, -5, -5, -5, -5, -5};
-        psqKing[1] = new int[]{-5, -5, -5, -5, -5, -5, -5, -5};
-        psqKing[2] = new int[]{-5, -5, -5, -5, -5, -5, -5, -5};
-        psqKing[3] = new int[]{-5, -5, -5, -5, -5, -5, -5, -5};
-        psqKing[4] = new int[]{-5, -5, -5, -5, -5, -5, -5, -5};
-        psqKing[5] = new int[]{-5, -5, -5, -5, -5, -5, -5, -5};
-        psqKing[6] = new int[]{-5, -5, -5, -5, -5, -5, -5, -5};
-        psqKing[7] = new int[]{-5, 10, 10, 0, 0, 10, 10, -5};
-        getLegalMoves(0, 0, true);
+    public static void initializePsq() {
+        psqPawn = new int[]  {
+            800,   800,   800,   800,   800,   800,  800,   800,
+            98, 134,  61,  95,  68, 126, 34, -11,
+            -6,   7,  26,  31,  65,  56, 25, -20,
+            -14,  13,   6,  21,  23,  12, 17, -23,
+            -27,  -2,  -5,  12,  17,   6, 10, -25,
+            -26,  -4,  -4, -10,   3,   3, 33, -12,
+            -35,  -1, -20, -23, -15,  24, 38, -22,
+            0,   0,   0,   0,   0,   0,  0,   0
+        };
+        psqKnight = new int[] {
+            -167, -89, -34, -49,  61, -97, -15, -107,
+            -73, -41,  72,  36,  23,  62,   7,  -17,
+            -47,  60,  37,  65,  84, 129,  73,   44,
+            -9,  17,  19,  53,  37,  69,  18,   22,
+            -13,   4,  16,  13,  28,  19,  21,   -8,
+            -23,  -9,  12,  10,  19,  17,  25,  -16,
+            -29, -53, -12,  -3,  -1,  18, -14,  -19,
+            -105, -21, -58, -33, -17, -28, -19,  -23,
+        };
+        psqBishop = new int[] {
+            -29,   4, -82, -37, -25, -42,   7,  -8,
+            -26,  16, -18, -13,  30,  59,  18, -47,
+            -16,  37,  43,  40,  35,  50,  37,  -2,
+            -4,   5,  19,  50,  37,  37,   7,  -2,
+            -6,  13,  13,  26,  34,  12,  10,   4,
+            0,  15,  15,  15,  14,  27,  18,  10,
+            4,  15,  16,   0,   7,  21,  33,   1,
+            -33,  -3, -14, -21, -13, -12, -39, -21,
+        };
+        psqRook = new int[] {
+            32,  42,  32,  51, 63,  9,  31,  43,
+            27,  32,  58,  62, 80, 67,  26,  44,
+            -5,  19,  26,  36, 17, 45,  61,  16,
+            -24, -11,   7,  26, 24, 35,  -8, -20,
+            -36, -26, -12,  -1,  9, -7,   6, -23,
+            -45, -25, -16, -17,  3,  0,  -5, -33,
+            -44, -16, -20,  -9, -1, 11,  -6, -71,
+            -19, -13,   1,  17, 16,  7, -37, -26,
+        };
+        psqQueen = new int[] {
+            -28,   0,  29,  12,  59,  44,  43,  45,
+            -24, -39,  -5,   1, -16,  57,  28,  54,
+            -13, -17,   7,   8,  29,  56,  47,  57,
+            -27, -27, -16, -16,  -1,  17,  -2,   1,
+            -9, -26,  -9, -10,  -2,  -4,   3,  -3,
+            -14,   2, -11,  -2,  -5,   2,  14,   5,
+            -35,  -8,  11,   2,   8,  15,  -3,   1,
+            -1, -18,  -9,  10, -15, -25, -31, -50,
+        };
+        psqKing = new int[] {
+            -65,  23,  16, -15, -56, -34,   2,  13,
+            29,  -1, -20,  -7,  -8,  -4, -38, -29,
+            -9,  24,   2, -16, -20,   6,  22, -22,
+            -17, -20, -12, -27, -30, -25, -14, -36,
+            -49,  -1, -27, -39, -46, -44, -33, -51,
+            -14, -14, -22, -46, -44, -30, -15, -27,
+            1,   7,  -8, -64, -43, -16,   9,   8,
+            -15,  36,  12, -54,   8, -28,  24,  14,
+        };
     }
 
     public static char makeMove(int x, int y, int x2, int y2, boolean realMove) {
@@ -202,37 +199,40 @@ public class Main {
 
     public static int pointValue(char piece, int x, int y, boolean newBotChanges) {
         if (piece == 'p') {
-            return 100 + psqPawn[7 - y][x];
+            return 100 + psqPawn[8*y+x];
         }
         if (piece == 'P') {
-            return 100 + psqPawn[y][x];
+            return 100 + psqPawn[(8*(7-y)+x)];
         }
         if (piece == 'n') {
-            return 320 + psqKnight[7 - y][x];
+            return 320 + psqKnight[8*y+x];
         }
         if (piece == 'N') {
-            return 320 + psqKnight[y][x];
+            return 320 + psqKnight[(8*(7-y)+x)];
         }
         if (piece == 'b') {
-            return 330 + psqBishop[7 - y][x];
+            return 330 + psqBishop[8*y+x];
         }
         if (piece == 'B') {
-            return 330 + psqBishop[y][x];
+            return 330 + psqBishop[(8*(7-y)+x)];
         }
         if (piece == 'r') {
-            return 500 + psqRook[y][x];
+            return 500 + psqRook[8*y+x];
         }
         if (piece == 'R') {
-            return 500 + psqRook[7 - y][x];
+            return 500 + psqRook[(8*(7-y)+x)];
         }
-        if (piece == 'q' || piece == 'Q') {
-            return 900 + psqQueen[y][x];
+        if (piece == 'q') {
+            return 900 + psqQueen[8*y+x];
+        }
+        if (piece == 'Q') {
+            return 900 + psqQueen[(8*(7-y)+x)];
         }
         if (piece == 'k') {
-            return 20000 + psqKing[y][x];
+            return 20000 + psqKing[8*y+x];
         }
         if (piece == 'K') {
-            return 20000 - psqKing[7 - y][x];
+            return 20000 + psqKing[(8*(7-y)+x)];
         }
         return 0;
     }
@@ -456,17 +456,15 @@ public class Main {
         for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 if (board[j][i] != ' ' && Character.isLowerCase(board[j][i]) == whiteToMove) {
-                    Vector<int[]> moves = movePiece(i, j,  whiteToMove, protection);
+                    Vector<int[]> moves = getLegalMoves(i, j,  whiteToMove);
                     for (int[] move : moves) {
                         char lastCaptured = makeMove(i, j, move[0], move[1], false);
-                        if (!inCheck( whiteToMove)) {
-                            if (!capture || lastCaptured != ' ') {
-                                if (lastCaptured == ' ') {
-                                    allMoves.add(new int[]{(int) board[move[1]][move[0]], i, j, move[0], move[1], scorePosition(whiteToMove, true)});
-                                }
-                                else {
-                                    allMoves.add(new int[]{(int) board[move[1]][move[0]], i, j, move[0], move[1], pointValue(lastCaptured, move[0], move[1], true ) - pointValue(board[move[1]][move[0]], i, j, true)});
-                                }
+                        if (!capture || lastCaptured != ' ') {
+                            if (lastCaptured == ' ') {
+                                allMoves.add(new int[]{(int) board[move[1]][move[0]], i, j, move[0], move[1], scorePosition(whiteToMove, true)});
+                            }
+                            else {
+                                allMoves.add(new int[]{(int) board[move[1]][move[0]], i, j, move[0], move[1], pointValue(lastCaptured, move[0], move[1], true ) - pointValue(board[move[1]][move[0]], i, j, true)});
                             }
                         }
                         unmakeMove(i, j, move[0], move[1], false, lastCaptured);
@@ -524,7 +522,7 @@ public class Main {
         return alpha;
     }
    
-    public static int search(boolean whiteToMove, int depth, int a, int b, boolean newBotChanges, int extensions) {
+    public static int search(boolean whiteToMove, int depth, int a, int b, boolean newBotChanges) {
         int alpha = a, beta = b;
         boolean extend = false;
         movesCalculated++;
@@ -536,9 +534,6 @@ public class Main {
             if (allMoves.size() == 0) {
                 return -99999999; 
             }
-            else if (extensions > 0) {
-                extend = true;
-            }
         }
         if (allMoves.size() == 0) {
             return 0;
@@ -547,10 +542,10 @@ public class Main {
             char lastCaptured = makeMove(move[1], move[2], move[3], move[4], false);
             int eval;
             if (extend) {
-                eval = -search(!whiteToMove, depth, -beta, -alpha, newBotChanges, extensions - 1);
+                eval = -search(!whiteToMove, depth, -beta, -alpha, newBotChanges);
             }
             else {
-                eval = -search(!whiteToMove, depth - 1, -beta, -alpha, newBotChanges, extensions);
+                eval = -search(!whiteToMove, depth - 1, -beta, -alpha, newBotChanges);
             } 
             int newMoveScore = scorePosition(whiteToMove, newBotChanges);
             unmakeMove(move[1], move[2], move[3], move[4], false, lastCaptured);
@@ -583,13 +578,7 @@ public class Main {
                 if (Instant.now().toEpochMilli() - startingTime < thinkingTime) {
                     int[] move = allMoves.get(pos);
                     char lastCaptured = makeMove(move[1], move[2], move[3], move[4], false);
-                    int evaluation;
-                    if (extend) {
-                        evaluation = -search(!whiteToMove, depth, -9999999, 9999999, newBotChanges, 0);
-                    }
-                    else {
-                        evaluation = -search(!whiteToMove, depth - 1, -9999999, 9999999, newBotChanges, 1);
-                    }
+                    int evaluation = -search(!whiteToMove, depth - 1, -9999999, 9999999, newBotChanges);
                     
                     if (evaluation >= maxScore || (optimalMove[0] == move[0] && optimalMove[1] == move[1] && optimalMove[2] == move[2] && optimalMove[3] == move[3] && optimalMove[4] == move[4])) {
                         maxScore = evaluation;
@@ -608,6 +597,7 @@ public class Main {
     }    
 
     public static void main(String[] args) {
+        initializePsq();
         JFrame frame = new JFrame("Chess Bot");
         frame.setSize(672 + 13, 672 + 38);
         frame.setResizable(false);
@@ -615,6 +605,16 @@ public class Main {
         frame.getContentPane().add(panel);
         frame.setVisible(true);
         frame.addMouseListener(new UserMouse());
+
+        board[0] = new char[]{'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'};
+        board[1] = new char[]{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'};
+        board[2] = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        board[3] = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        board[4] = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        board[5] = new char[]{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        board[6] = new char[]{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'};
+        board[7] = new char[]{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'};
+        getLegalMoves(0, 0, true);
 
         boolean whiteToMove = true;
         //gameloop
